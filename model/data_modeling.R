49ad71f7fedcd95fefa672db8992c6a00761f2f3
jingxchen <54999902+jingxchen@users.noreply.github.com>
2019-12-07 00:24:21 -0500

    data modeling with parallel computing_R
    written by jingxian, 2.0 version, Dec 7th, 2019

--- model/data_modeling.R
+++ model/data_modeling.R
@@ -1,17 +1,29 @@
 ################################################################################
 # Title: Model establishing for Group project 506
 # Author: Jingxian Chen
-# Version: 1.0
-# Date: Dec 6th, 2019
+# Version: 2.0
+# Date: Dec 7th, 2019
 ################################################################################
 ## load data
 source("C:/Users/95260/Desktop/data_preprocessing.R")
 
 ### Modeling part
-library(glmnet)
-library(ROCit)
 library(doParallel)
-N_test <- 100
+
+## setup parallel computing parameter
+ncores=2   
+
+# set up a cluster called 'cl'
+cl = makeCluster(ncores)
+
+# register the cluster
+registerDoParallel(cl)
+
+
+# set the number of testset partition times
+N_test <- 10
+
+# delete the insurance variable and add interactive term with intake variables
 times_insur <- function(x) x * Data_final$insurance
 Data_final <- select(Data_final, -weight) %>%
     mutate_at(c("iron", "calcium", "zinc", "sodium", "VA", "VC", "VE",
@@ -21,67 +33,79 @@ Data_final <- select(Data_final, -weight) %>%
                                   "age_group", "income", "diabetes",
                                   "insurance"), suffix = c("_ins", "_ogn")) %>%
     select(-c("id", "weight", "insurance"))
+
+# get the number of obs
 Nnum <- nrow(Data_final)
+
+# the number of obs as test data
 test_Num <- round(0.2 * Nnum)
+
+# the number of obs as train and cv data
 train_Num <- Nnum - test_Num
 
+# create data structure to store the optimal lambda from cross validation and 
+# the AUC value for evaluzting model performance
+#result <- matrix(NA, nrow = N_test, ncol = 2)
 
-lambda <- vector()
-beta <- matrix(NA, ncol = N_test, nrow = 46)
-AUC <- vector()
-for(i in 1:N_test){
+# building the model for N_test time in order to avoid overfitting problem
+#t1 = proc.time()
+result = foreach(i = 1:N_test) %dopar% {
+    library(glmnet)
+    library(ROCit)
+    # divide data into training set and test set
     set.seed(50*i)
     test_id <- sample(1:Nnum, test_Num)
     test_data <- Data_final[test_id,]
     train_cv_data <- Data_final[-test_id,]
     
     
-    ### build model
+    # divide the dependent variables and response in the training set
     x_train <- model.matrix(diabetes ~ ., train_cv_data)[, -1]
     y_train <- train_cv_data$diabetes    
     
-    ### choose lambda using cv
-    cv.lasso <- cv.glmnet(x_train, y_train, alpha = 1, family = "binomial", type.measure = "auc")
-    ## use the lambda which minimize the trainning error
-    #   model_min <- glmnet(x_train, y_train, alpha = 1, famaily = "binomial", 
-    #                   lambda = cv.lasso$lambda.min)
-    #   beta_min <- coef(model_min)
+    # use cross validation metthod to find the optimal lambda parameter
+    cv.lasso <- cv.glmnet(x_train, y_train, alpha = 1, family = "binomial", 
+                          type.measure = "auc", parallel = TRUE)
     
-    ## use the lambda which gives the best trade off on the model complex level and error
+    # use the lambda which gives the best AUC value and avoid improving model 
+    # complexity to build our model
     model_small <- glmnet(x_train, y_train, alpha = 1, famaily = "binomial", 
                           lambda = cv.lasso$lambda.1se)
-    lambda[i] <- cv.lasso$lambda.1se
-    beta <- as.matrix(coef(model_small))
     
-    ### make prediction
+    # store the optimal lambda value
+    lambda <- cv.lasso$lambda.1se
+    
+    # make prediction using test data
     x_test <- model.matrix(diabetes ~ ., test_data)[, -1]
     y_test <- test_data$diabetes
     y_hat_small <- predict.glmnet(model_small, newx = x_test, type = "response")
-    #   y_hat_min <- predict.glmnet(model_min, newx = x_test, type = "response")            
     
-    ### compute accuracy using ROC curve
+    # evaluate model performance on test data using ROC curve and AUC value
     ROC_obj_small <- rocit(score = as.vector(y_hat_small), class = y_test)
-    plot(ROC_obj_small)
-    AUC[i] <- ROC_obj_small$AUC    
     
-    #    ROC_obj_min <- rocit(score = as.vector(y_hat_min), class = y_test)
-    #    plot(ROC_obj_min)
-    #    AUC_min <- ROC_obj_min$AUC
-    
-    #    cat(AUC_small, AUC_min)
-    cat(i," done!\n") 
+    # store the AUC value and plot the ROC curve
+    # plot(ROC_obj_small)
+    AUC <- ROC_obj_small$AUC    
+    c(lambda, AUC)
 }  
+#t2 = proc.time() - t1
+#t2
 
-
-
-## final model for all data we have
+# building final model for all of our data 
+result <- matrix(unlist(result1), ncol = N_test, nrow = 2)
 x_all <- model.matrix(diabetes~., Data_final)[, -1]
 y_all <- Data_final$diabetes
-lambda_final <- mean(lambda)
+
+# choose the mean value of the optimal lambdas above as the final optimal lambda
+# for our final model
+lambda_final <- rowSums(result)[1]
 model <- glmnet(x_all, y_all, family = "binomial", alpha = 1, lambda = lambda_final)
+
+# the coefficients of the final model
 beta_all <- coef(model)
 
-## estimate AUC for final model
-AUC_final <- mean(AUC)
+# estimation of the final model performance using the average AUC value above
+AUC_final <- rowSums(result)[2]
 
 
+stopCluster(cl)
